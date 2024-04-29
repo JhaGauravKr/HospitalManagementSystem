a419b823d8d35b63a98fbcddea09cb8f563d3cec
JhaGauravKr <144140001+JhaGauravKr@users.noreply.github.com>
2024-04-29 06:29:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+#Tushar Borole
+#Python 2.7
+
+from flask import Flask,send_from_directory,render_template
+from flask_restful import Resource, Api
+from package.patient import Patients, Patient
+from package.doctor import Doctors, Doctor
+from package.appointment import Appointments, Appointment
+from package.common import Common
+import json
+
+
+with open('config.json') as data_file:
+    config = json.load(data_file)
+
+app = Flask(__name__, static_url_path='')
+api = Api(app)
+
+api.add_resource(Patients, '/patient')
+api.add_resource(Patient, '/patient/<int:id>')
+api.add_resource(Doctors, '/doctor')
+api.add_resource(Doctor, '/doctor/<int:id>')
+api.add_resource(Appointments, '/appointment')
+api.add_resource(Appointment, '/appointment/<int:id>')
+api.add_resource(Common, '/common')
+
+# Routes
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host=config['host'],port=config['port'])
